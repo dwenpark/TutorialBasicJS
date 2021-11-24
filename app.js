0c5e17326f707bec2b0d3f52a85426ffa333edd2
dwenpark <dwenup@gmail.com>
2021-11-24 23:16:23 +0900

    4.0

--- app.js
+++ app.js
@@ -1,71 +1,8 @@
-const a = 5;
-const b = 2;
-let myName = "dwen";
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+const loginButton = loginForm.querySelector("#login-form button");
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log(myName);
-
-myName = "JG";
-console.log(myName);
-
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-
-// Get Item from Array
-console.log(daysOfWeek);
-
-// Add one more day to the array
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
-
-const player = {
-	name: "dwen",
-	age: 30,
-	fat: true
+function onLoginButtonClick() {
+	console.log(loginInput.value)
 }
-
-player.age = 29
-
-console.log(player)
-
-function sayHello(nameOfPerson, age) {
-	console.log("Hello" + nameOfPerson + "I'm" + age);
-}
-
-sayHello("dwen", 30)
-
-function plus(a, b) {
-	console.log(a + b);
-}
-
-plus(5, 5)
-
-const calculator = {
-	add: function (a, b) {
-		return (a + b);
-	},
-	minus: function (a, b) {
-		return (a - b);
-	},
-	times: function (a, b) {
-		return (a * b);
-	},
-	divide: function (a, b) {
-		return (a / b);
-	},
-	power: function (a, b) {
-		return (a ** b);
-	},
-
-}
-
-const age = 96;
-
-function calculateKrAge(ageOfForeigner) {
-	return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
+loginButton.addEventListener("click", onLoginButtonClick)
