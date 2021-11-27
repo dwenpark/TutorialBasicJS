48b96b931c814b9320cb822d0943bd5acd9136f4
dwenpark <dwenup@gmail.com>
2021-11-27 22:03:05 +0900

    4.6

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
-const HIDDEN_CLASSNAME = "hidden"
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
 	event.preventDefault();
 	const username = loginInput.value;
 	loginForm.classList.add(HIDDEN_CLASSNAME);
-	greeting.innerText = "Hello " + username;
+	localStorage.setItem(USERNAME_KEY, username);
+	paintGreetings(username);
+}
+
+function paintGreetings(username) {
+	greeting.innerText = `Hello ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const saveUsername = localStorage.getItem(USERNAME_KEY)
+
+if (saveUsername === null) {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+	paintGreetings(saveUsername);
+}
